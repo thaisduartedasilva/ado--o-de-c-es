4e5d284e2b8b7f099ff15f1a0546499834bbcfd5
thaisduarte <thais_duarte-silva@estudante.sesisenai.org.br>
2026-04-06 12:50:50 -0300

    add - validação

--- script.js
+++ script.js
@@ -5,6 +5,15 @@ document.getElementById(formularioAdocao).addEventListener("submit", function(e)
     let email = document.getElementById("email").value; 
     let moradia = document.getElementById("moradia").value; 
     let quintal = document.querySelector('input[name="quintal"]:checked');
+    let telefone = document.getElementById("telefone").value;
+    let CPF = document.getElementById("CPF").value;
+    let idade = document.getElementById("idade").value;
+    let cidade = document.getElementById("cidade").value;
+    let pet = document.querySelector('input[name="pet"]:checked');
+    let horasSozinho = document.getElementById("horasSozinho").value;
+    let motivo = document.getElementById("motivo").value;
+    let responsabilidade = document.getElementById("responsabilidade").value;
+
     
     if(nome.length < 3) return alert("Nome Inválido!");
     // como faz o @?????
@@ -16,6 +25,7 @@ document.getElementById(formularioAdocao).addEventListener("submit", function(e)
     //possui quintal obrigatório????????
     //ja teve pet obrigatóio???
     if(horasSozinho = NaN) return alert("Valor númerico inválido!"); // ta errado
+    if(horasSozinho > 8) return alert ("Você não pode adotar! Você fica muito tempo fora de casa!"); //Solicitar justificativa adicional
     if(motivo.length < 10) return alert("Motivo da adoção inválido!");
     // termo deve estar marcado ?????????
 
