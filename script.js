2c13b88550e0f2beb203ab3a06425b62252380cb
thaisduarte <thais_duarte-silva@estudante.sesisenai.org.br>
2026-04-08 12:43:03 -0300

    add - sos

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-document .getElementById("formAdocao").addEventListener("submit", function (e) {
+document.getElementById("formAdocao").addEventListener("submit", function (e) {
     e.preventDefault();
 
-    let nome = document.getElementById("nome").value; 
-    let email = document.getElementById("email").value; 
-    let moradia = document.getElementById("moradia").value; 
+    let nome = document.getElementById("nome").value;
+    let email = document.getElementById("email").value;
+    let moradia = document.getElementById("moradia").value;
     let quintal = document.querySelector('input[name="quintal"]:checked');
     let telefone = document.getElementById("telefone").value;
     let CPF = document.getElementById("CPF").value;
@@ -14,34 +14,77 @@ document .getElementById("formAdocao").addEventListener("submit", function (e) {
     let motivo = document.getElementById("motivo").value;
     let responsabilidade = document.getElementById("responsabilidade").value;
 
-    
-    if(nome.length < 3) return alert("Nome Inválido!");
-    if(!email.includes("@")) return alert("Email inválido!");
-    if(telefone.length < 8) return alert("Número de telefone inválido!");
-    if(CPF === "") return alert("CPF obrigatório!");//
-    if(idade < 18) return alert("Você ainda é menor de idade!");
-    if(cidade === "") return alert("Cidade obrigatória!");
-    if(moradia === "") return alert("Selecione o tipo de moradia!");
-    if(!quintal) return alert("Informe se possui quintal!");
-    if(!pet) return alert("Informe se já teve pet!");
-    if(isNaN(horasSozinho)) return alert("Valor numérico inválido!");
-    if(horasSozinho > 8){
+
+    if (nome.length < 3) return alert("Nome Inválido!");
+    if (!email.includes("@")) return alert("Email inválido!");
+    if (telefone.length < 8) return alert("Número de telefone inválido!");
+    if (CPF === "") return alert("CPF obrigatório!");
+
+    let cpfsExistentes = ["12345678900", "11111111111"];
+    if(cpfsExistentes.includes(CPF)) {
+        return alert("CPF já cadastrado! Informar um CPF válido!");
+    }
+
+    if (idade < 18) return alert("Você ainda é menor de idade!");
+    if (cidade === "") return alert("Cidade obrigatória!");
+
+    if (moradia === "") return alert("Selecione o tipo de moradia!");
+
+    if (moradia === "apartamento") {
+        if (quintal === "sim") {
+            return alert("Você mora em apartamento, você não tem quintal!");// Quem mora em apartamento não pode indicar que possui quintal
+
+        }
+    }
+    if (!quintal) return alert("Informe se possui quintal!");
+    if (!pet) return alert("Informe se já teve pet!");
+    if (isNaN(horasSozinho)) return alert("Valor numérico inválido!");
+    if (horasSozinho > 8) {
         let justificativa = prompt("O animal ficará muito tempo sozinho. Justifique:");
-        if(!justificativa || justificativa.length < 5){
+        if (!justificativa || justificativa.length < 5) {
             return alert("Justificativa insuficiente!");
         }
     }
-    if(motivo.length < 10) return alert("Motivo da adoção inválido!");
+    if (motivo.length < 10) return alert("Motivo da adoção inválido!");
+    if (motivo.includes("hoje") || motivo.includes("agora")) {
+        alert("Decisão impulsiva! Você não pode adotar um pet!");
+    }
 
     let motivosInvalidos = ["quero", "porque sim", "sim", "gosto"];
-    if(motivosInvalidos.includes(motivo)){
+    if (motivosInvalidos.includes(motivo)) {
         return alert("O motivo da adoção não pode ser genérico!");
     }
-    if(!responsabilidade.checked) return alert("Você deve aceitar os termos de responsabilidade!");
-    if(pet.value === "nao"){
+    
+    if (responsabilidade === false) return alert("Você deve aceitar os termos de responsabilidade!");//
+
+    if (pet.value === "nao") {
         alert("Você nunca teve pets antes. A ONG devera acompanhar sua adaptação com o pet.");
     }
 
-    document.getElementById("resultado"). innerHTML = "Cadastro realizado com sucesso! <br>" + "Nome: " + nome;
+    let salario = prompt("Informe o seu salário: ");
+    if (salario <= 1000) {
+        return alert("Salário insuficiente! Você não tem codições de ter um pet!");
+    } else {
+        return alert("Você tem condições financeiras de ter um pet!");
+    }
+
+    document.getElementById("resultado").innerHTML = "Cadastro realizado com sucesso !< br>" + "Nome: " + nome;
+
+
+});
+
+document.getElementById("moradia").addEventListener("change", function(e) {
+
+    let aceitaPet = document.getElementById("aceitaPet");
+
+    if (this.value === "apartamento") {
+        aceitaPet.innerHTML = `
+            <label>O apartamento aceita pets?</label><br>
+            <input type="radio" name="aceitaPet" value="sim"> Sim
+            <input type="radio" name="aceitaPet" value="nao"> Não
+        `;
+    } else {
+        aceitaPet.innerHTML = "";
+    }
 
 });
